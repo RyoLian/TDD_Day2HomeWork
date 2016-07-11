639da36f9164bb49a85cde7a66f351d783e823e3
s01lyh <s01lyh@S01LYH-NB>
2016-07-11 11:25:00 +0800

    第一集買一本 紅燈

---
+++ PotterShoppingCart/PotterShoppingCart.cs/PotterShoppingCart_Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PotterShoppingCart.cs
+{
+    public class PotterShoppingCart_Class
+    {
+        public Dictionary<string, int> _PoterSet = new Dictionary<string, int>
+        {
+            ["HarryPoter_1"] = 0,
+            ["HarryPoter_2"] = 0,
+            ["HarryPoter_3"] = 0,
+            ["HarryPoter_4"] = 0,
+            ["HarryPoter_5"] = 0
+        };
+
+
+        public void BuyFirst(int v)
+        {
+            _PoterSet["HarryPoter_1"] += v;
+        }
+
+        public object Caltotal()
+        {
+            return 0;
+        }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCartTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart.cs;
+
+namespace PotterShoppingCartTest
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        [TestMethod]
+        public void PotterShoppingCart_只買第一集一本_價格100元()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
