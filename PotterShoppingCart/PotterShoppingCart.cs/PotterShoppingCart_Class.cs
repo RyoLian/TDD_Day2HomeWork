48ba050eb9dcaa8e68cdf5bbadf59f80706d4af9
s01lyh <s01lyh@S01LYH-NB>
2016-07-11 14:10:29 +0800

    綠燈+重購

--- PotterShoppingCart/PotterShoppingCart.cs/PotterShoppingCart_Class.cs
+++ PotterShoppingCart/PotterShoppingCart.cs/PotterShoppingCart_Class.cs
@@ -1,29 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PotterShoppingCart.cs
 {
     public class PotterShoppingCart_Class
     {
-        public Dictionary<string, int> _PoterSet = new Dictionary<string, int>
+        public Dictionary<string, int> _PotterSet = new Dictionary<string, int>
         {
-            ["HarryPoter_1"] = 0,
-            ["HarryPoter_2"] = 0,
-            ["HarryPoter_3"] = 0,
-            ["HarryPoter_4"] = 0,
-            ["HarryPoter_5"] = 0
+            ["HarryPotter_1"] = 0,
+            ["HarryPotter_2"] = 0,
+            ["HarryPotter_3"] = 0,
+            ["HarryPotter_4"] = 0,
+            ["HarryPotter_5"] = 0
         };
-
 
         public void BuyFirst(int v)
         {
-            _PoterSet["HarryPoter_1"] += v;
+            _PotterSet["HarryPotter_1"] += v;
         }
 
-        public object Caltotal()
+        public void BuySecond(int v)
         {
-            return 0;
+            _PotterSet["HarryPotter_2"] += v;
+        }
+
+        public void BuyThird(int v)
+        {
+            _PotterSet["HarryPotter_3"] += v;
+        }
+
+        public void BuyForth(int v)
+        {
+            _PotterSet["HarryPotter_4"] += v;
+        }
+
+        public void BuyFifth(int v)
+        {
+            _PotterSet["HarryPotter_5"] += v;
+        }
+
+        public decimal Caltotal()
+        {
+            var totalPay = 0;
+            totalPay = getTotalMoney(_PotterSet.Values.ToArray());
+
+            return totalPay;
+        }
+
+        private int getTotalMoney(int[] Values)
+        {
+            var Kind = 1;
+            var totalPay = 0;
+
+            while (Kind > 0)
+            {
+                Kind = 0;
+                for (int i = 0; i <= Values.Length - 1; i++)
+                {
+                    if (Values[i] > 0)
+                    {
+                        Kind += 1;
+                        Values[i] -= 1;
+                    }
+                }
+                totalPay += getMoney(Kind);
+            }
+            return totalPay;
+        }
+
+        private int getMoney(int Kind)
+        {
+            switch (Kind)
+            {
+                case 1: //買1種=100
+                    return 100;
+                case 2: //買2種=190
+                    return 190;
+                case 3: //買3種=270
+                    return 270;
+                case 4: //買4種=320
+                    return 320;
+                case 5: //買5種=375
+                    return 375;
+                default:
+                    return 0;
+            }
         }
     }
 }
--- PotterShoppingCart/PotterShoppingCart/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCartTest.cs
@@ -21,5 +21,132 @@ namespace PotterShoppingCartTest
             Assert.AreEqual(expected, actual);
         }
 
+
+        [TestMethod]
+        public void 第一集買一本_第二集也買一本_價格應為_190()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(1);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 一二三集各買了一本_價格應為_270()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(1);
+            cal.BuyThird(1);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 270;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 一二三四集各買了一本_價格應為_320()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(1);
+            cal.BuyThird(1);
+            cal.BuyForth(1);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 320;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void 一次買了整套_一二三四五集各買了一本_價格應為_375()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(1);
+            cal.BuyThird(1);
+            cal.BuyForth(1);
+            cal.BuyFifth(1);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 375;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void 一二集各買了一本_第三集買了兩本_價格應為_370()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(1);
+            cal.BuyThird(2);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 370;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void 第一集買了一本_第二三集各買了兩本_價格應為_460()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(2);
+            cal.BuyThird(2);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 460;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void 第一集_1_第二集_3_第三集_3_第四集_5_第五集_2_應為1165()
+        {
+            //arrange
+            var cal = new PotterShoppingCart_Class();
+            cal.BuyFirst(1);
+            cal.BuySecond(3);
+            cal.BuyThird(3);
+            cal.BuyForth(5);
+            cal.BuyFifth(2);
+
+            //act
+            var actual = cal.Caltotal();
+
+            //assert
+            var expected = 1165;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
